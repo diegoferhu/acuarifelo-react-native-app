573f8ded057b9b9e6807fe64b4918254dcca8aca
Diego Hurtado <diegoferhu@gmail.com>
2021-07-14 09:11:55 -0500

    :sparkles: Add styles home temperature view

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,14 +1,35 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-
+import {View, Text, StyleSheet} from 'react-native';
+import colors from '../constants/colors';
 class HomeScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>Home screen</Text>
+      <View style={styles.temperature__container}>
+        <Text style={styles.temperature__title}>Temperatura</Text>
+        <Text style={styles.temperature__text}>24 º</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  temperature__container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  temperature__title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: colors.blackPearl,
+    textAlign: 'center',
+  },
+  temperature__text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: colors.blackPearl,
+    textAlign: 'center',
+  },
+});
 
 export default HomeScreen;
--- src/pages/HomeStack.js
+++ src/pages/HomeStack.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreens from './HomeScreen';
+import colors from './../constants/colors';
 
 const Stack = createStackNavigator();
 
 const HomeStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreens} />
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: colors.blackPearl,
+        headerStyle: {backgroundColor: colors.zircon},
+      }}>
+      <Stack.Screen name="Acuarifelo" component={HomeScreens} />
     </Stack.Navigator>
   );
 };
