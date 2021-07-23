07ff49fd0386791ab4aa9c63073eecce4513b2dc
Diego Hurtado <diegoferhu@gmail.com>
2021-07-23 17:45:55 -0500

    :sparkles: Connect to socket and catch temperature event

--- src/pages/Home/HomeScreen.js
+++ src/pages/Home/HomeScreen.js
@@ -1,7 +1,23 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import colors from '../../constants/colors';
+import socketIOClient from 'socket.io-client';
+const ENDPOINT = 'http://192.168.68.112:5000/';
 class HomeScreen extends Component {
+  componentDidMount() {
+    const socket = socketIOClient(ENDPOINT);
+    console.log('componentDidMount: ');
+
+    socket.on('connect', function () {
+      console.log('get temperature');
+      socket.emit('get temperature', {data: "I'm connected!"});
+    });
+
+    socket.on('temperature', data => {
+      console.log('temperature: ', data);
+    });
+  }
+
   render() {
     return (
       <View style={styles.temperature__container}>
